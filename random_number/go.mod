a0181c8902d70f1d8990e1f6ac22e14f3342afc1
ituserxxx <ituserxxx@gmail.com>
2023-12-08 16:24:42 +0800

    v1

--- random_number/go.mod
+++ random_number/go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/gorilla/websocket v1.5.1
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	gopkg.in/antage/eventsource.v1 v1.0.0-20150318155416-803f4c5af225
 )
 
 require golang.org/x/net v0.17.0 // indirect
--- random_number/main.go
+++ random_number/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/websocket"
 	qrcode "github.com/skip2/go-qrcode"
-	"io/ioutil"
+	"gopkg.in/antage/eventsource.v1"
 	"log"
 	"net/http"
 	"os"
@@ -13,7 +12,12 @@ import (
 	"sync"
 )
 
+var sseHandler eventsource.EventSource
+
 func main() {
+	// 开启一个 sse
+	sseHandler = eventsource.New(nil, nil)
+	defer sseHandler.Close()
 	fmt.Println("http://ip:8001")
 	// 启动HTTP服务器
 	http.ListenAndServe(":8001", RegisterRoute())
@@ -26,7 +30,6 @@ func cacheDemo() {
 	cache.Store("key", "value")
 	// 获取缓存
 	value, found := cache.Load("key")
-
 	if found {
 		fmt.Println(value)
 	}
@@ -43,27 +46,26 @@ func qrcodeDemo() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println("二维码生成成功！")
 }
 
+func SendSee(id, v string) {
+	sseHandler.SendEventMessage(v, id, id)
+}
 func RegisterRoute() http.Handler {
 	router := http.NewServeMux()
 	handler := corsMiddleware(router)
-	fs := http.FileServer(http.Dir("dist"))
 	//建立路由规则，将所有请求交给静态文件处理器处理
-	router.Handle("/cj_qrcode", fs)
-	// GET请求处理
-
-	// ws 请求
-	router.HandleFunc("/ws", middleware(wsHandler))
-
+	router.Handle("/", http.FileServer(http.Dir("./")))
+	router.Handle("/cj_qrcode", http.FileServer(http.Dir("./cj_qrcode/")))
+	router.Handle("/events", sseHandler)
 	// POST请求处理
 	router.HandleFunc("/hello", middleware(apiHandlerTest))                                      // 测试
 	router.HandleFunc("/api/get_cj_info", middleware(Crontroller.HandlerGetCjInfo))              // 获取抽奖信息
 	router.HandleFunc("/api/save_cj_info", middleware(Crontroller.HandlerSaveCjInfo))            // 保存抽奖信息
 	router.HandleFunc("/api/save_gs_name", middleware(Crontroller.HandlerSaveGsName))            // 保存公司名称
-	router.HandleFunc("/api/get_join_cj_number", middleware(Crontroller.HandlerJoinCj))          // 参与抽奖
+	router.HandleFunc("/api/user_view_cj", middleware(Crontroller.HandlerUserViewCj))            // 用户查看抽奖
+	router.HandleFunc("/api/user_join_cj", middleware(Crontroller.HandlerUserJoinCj))            // 用户参与抽奖
 	router.HandleFunc("/api/send_cj_dyamic_msg", middleware(Crontroller.HandlerSendCjDyamicMsg)) // 发送抽奖互动消息
 	return handler
 }
@@ -86,136 +88,225 @@ type cj struct {
 	GsName       string     `json:"gs_name"`
 	CjQrcode     string     `json:"cj_qrcode"`
 	L            []cjOption `json:"l"`
-	JoinUserList []JoinUser // 参与用户列表
-	PersonTotal  int        `json:"person_total"` //当前参与人数
+	JoinUserList []JoinUser `json:"join_user_list"` // 参与用户列表
+	PersonTotal  int        `json:"person_total"`   //当前参与人数
 	Lock         sync.RWMutex
 }
 
 var cjManager = map[string]*cj{}
 var cjMLock sync.RWMutex
 
-func (uc *uLogic) HandlerGetCjInfo(w http.ResponseWriter, r *http.Request) {
-	var info = &cj{}
+type GetCjInfoResp struct {
+	GsName       string     `json:"gs_name"`
+	CjQrcode     string     `json:"cj_qrcode"`
+	L            []cjOption `json:"l"`
+	PersonTotal  int        `json:"person_total"` //当前参与人数
+	JoinUserList []JoinUser `json:"join_user_list"`
+}
 
+// HandlerGetCjInfo 发起人才能进的抽奖信息页面
+func (uc *uLogic) HandlerGetCjInfo(w http.ResponseWriter, r *http.Request) {
+	var res = &GetCjInfoResp{
+		L:            make([]cjOption, 0),
+		JoinUserList: make([]JoinUser, 0),
+	}
 	uuid := r.Header.Get("uuid")
+	v, ok := cjManager[uuid]
 	// 当前抽奖不存在
-	if v, ok := cjManager[uuid]; ok == false { // 活动发起人的 uuid == lid  抽奖id
+	if ok == false { // 活动发起人的 uuid == lid  抽奖id
+		var info = &cj{
+			L:            make([]cjOption, 0),
+			JoinUserList: make([]JoinUser, 0),
+		}
 		info.CJId = uuid
 		info.CjQrcode = getQrcodeUrl(uuid)
 		cjMLock.Lock()
 		defer cjMLock.Unlock()
 		cjManager[uuid] = info
-	} else {
-		info = v
+		res.CjQrcode = info.CjQrcode
 	}
-	respOk(w, info)
+	// 当前抽奖存在
+	if ok == true {
+		res.CjQrcode = v.CjQrcode
+		res.PersonTotal = v.PersonTotal
+		res.L = v.L
+		res.GsName = v.GsName
+		res.JoinUserList = v.JoinUserList
+	}
+	respOk(w, res)
+}
+
+type SaveCjInfoReq struct {
+	L []cjOption `json:"l"`
 }
 
 func (uc *uLogic) HandlerSaveCjInfo(w http.ResponseWriter, r *http.Request) {
-	// 读取请求体数据
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		respErr(w, "parse req data err "+err.Error())
-		return
-	}
 	// 解析请求体数据到结构体
-	var info *cj
-	err = json.Unmarshal(body, &info)
+	var list *SaveCjInfoReq
+	err := json.NewDecoder(r.Body).Decode(&list)
 	if err != nil {
 		respErr(w, "json unmarshal err "+err.Error())
 		return
 	}
 	uuid := r.Header.Get("uuid")
-	if v, ok := cjManager[uuid]; ok == false && info != nil {
-		cjManager[info.CJId] = info
-	} else {
+	v, ok := cjManager[uuid]
+	if ok == false {
+		respErr(w, "抽奖不存在~~")
+		return
+	}
+	if ok == true {
 		v.Lock.RLock()
 		defer v.Lock.RUnlock()
-		v.GsName = info.GsName
-		v.L = info.L
+		v.L = list.L
 	}
 	respOk(w, "")
 }
+
+type saveGsNameReq struct {
+	GsName string `json:"gs_name"`
+}
+
 func (uc *uLogic) HandlerSaveGsName(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	var req saveGsNameReq
+	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		respErr(w, "parse req data err "+err.Error())
+		respErr(w, "json unmarshal err "+err.Error())
 		return
 	}
-
-	// 获取 POST 参数
-	name := strings.TrimSpace(r.Form.Get("gs_name"))
-	if len(name) == 0 {
+	req.GsName = strings.TrimSpace(req.GsName)
+	if len(req.GsName) == 0 {
 		respErr(w, "活动名称不能为空")
 		return
 	}
 	uuid := r.Header.Get("uuid")
-	if v, ok := cjManager[uuid]; ok == false {
-		respErr(w, "cj 不存在 "+err.Error())
+	v, ok := cjManager[uuid]
+	if ok == false {
+		respErr(w, "抽奖不存在~~")
 		return
-	} else {
+	}
+	if ok == true {
 		v.Lock.RLock()
 		defer v.Lock.RUnlock()
-		v.GsName = name
+		v.GsName = req.GsName
 	}
 	respOk(w, "")
 }
-func (uc *uLogic) HandlerJoinCj(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+
+type JoinCjReq struct {
+	LID string `json:"lid"`
+}
+
+type UserViewCjResp struct {
+	Num    int    `json:"num"`
+	GsName string `json:"gs_name"`
+}
+
+func (uc *uLogic) HandlerUserViewCj(w http.ResponseWriter, r *http.Request) {
+	var req JoinCjReq
+	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		respErr(w, "parse req data err "+err.Error())
+		respErr(w, "json unmarshal err "+err.Error())
 		return
 	}
-	// 获取 POST 参数
-	lid := strings.TrimSpace(r.Form.Get("lid"))
-	if len(lid) == 0 {
+
+	req.LID = strings.TrimSpace(req.LID)
+	if len(req.LID) == 0 {
 		respErr(w, "活动id不能为空")
 		return
 	}
 	uuid := r.Header.Get("uuid")
-	if v, ok := cjManager[lid]; ok == false {
-		respErr(w, "cj 不存在 "+err.Error())
+	v, ok := cjManager[req.LID]
+	if ok == false {
+		respErr(w, "抽奖不存在~~")
 		return
-	} else {
-		v.Lock.RLock()
-		defer v.Lock.RUnlock()
-		v.PersonTotal += 1
+	}
+	if ok == true {
+		var userNum int
 		for _, u := range v.JoinUserList {
 			if u.UUID == uuid {
-				respOk(w, "")
-				return
+				userNum = u.CjNumber
+				break
 			}
 		}
-		v.JoinUserList = append(v.JoinUserList, JoinUser{
-			UUID:     uuid,
-			CjNumber: v.PersonTotal,
+		respOk(w, UserViewCjResp{
+			Num:    userNum,
+			GsName: v.GsName,
 		})
 	}
-	respOk(w, "")
 }
-func (uc *uLogic) HandlerSendCjDyamicMsg(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+func (uc *uLogic) HandlerUserJoinCj(w http.ResponseWriter, r *http.Request) {
+	var req JoinCjReq
+	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		respErr(w, "parse req data err "+err.Error())
+		respErr(w, "json unmarshal err "+err.Error())
 		return
 	}
-	// 获取 POST 参数
-	lid := strings.TrimSpace(r.Form.Get("lid"))
-	if len(lid) == 0 {
+
+	req.LID = strings.TrimSpace(req.LID)
+	if len(req.LID) == 0 {
 		respErr(w, "活动id不能为空")
 		return
 	}
-	msg := strings.TrimSpace(r.Form.Get("msg"))
-	if len(msg) == 0 {
+	uuid := r.Header.Get("uuid")
+
+	v, ok := cjManager[req.LID]
+	if ok == false {
+		respErr(w, "抽奖不存在~~")
+		return
+	}
+	if ok == true {
+		v.Lock.RLock()
+		defer v.Lock.RUnlock()
+		var isJoin bool
+		var userNum int
+		for _, u := range v.JoinUserList {
+			if u.UUID == uuid {
+				isJoin = true
+				userNum = u.CjNumber
+				break
+			}
+		}
+		if isJoin == false {
+			v.PersonTotal += 1
+			v.JoinUserList = append(v.JoinUserList, JoinUser{
+				UUID:     uuid,
+				CjNumber: v.PersonTotal,
+			})
+			userNum = v.PersonTotal
+			SendSee(v.CJId, fmt.Sprintf("join-用户%s刚刚加入", uuid))
+		}
+		respOk(w, userNum)
+	}
+}
+
+type SendCjDyamicMsgReq struct {
+	LID string `json:"lid"`
+	Msg string `json:"msg"`
+}
+
+func (uc *uLogic) HandlerSendCjDyamicMsg(w http.ResponseWriter, r *http.Request) {
+	var req SendCjDyamicMsgReq
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		respErr(w, "json unmarshal err "+err.Error())
+		return
+	}
+	req.LID = strings.TrimSpace(req.LID)
+	if len(req.LID) == 0 {
+		respErr(w, "活动id不能为空")
+		return
+	}
+	req.Msg = strings.TrimSpace(req.Msg)
+	if len(req.Msg) == 0 {
 		respErr(w, "")
 		return
 	}
 	uuid := r.Header.Get("uuid")
 	// 获取当当前用户的编号
 	var uNumber int
-	v, ok := cjManager[lid]
+	v, ok := cjManager[req.LID]
 	if ok == false {
-		respErr(w, "cj 不存在 "+err.Error())
+		respErr(w, "抽奖不存在~~")
 		return
 	}
 	for _, u := range v.JoinUserList {
@@ -228,11 +319,8 @@ func (uc *uLogic) HandlerSendCjDyamicMsg(w http.ResponseWriter, r *http.Request)
 		respErr(w, "请先参与该活动")
 		return
 	}
-	_, isTrue := Manager.Clients[lid]
-	if isTrue {
-		SendToWeb(lid, msg)
-	}
-
+	SendSee(v.CJId, fmt.Sprintf("msg-用户%s说：", req.Msg))
+	respOk(w, "")
 }
 
 // 自定义中间件函数
@@ -252,18 +340,14 @@ func middleware(next http.HandlerFunc) http.HandlerFunc {
 }
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 设置允许跨域的域名列表
-		// 设置允许的请求头部
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,X-token,uuid")
-
+		w.Header().Set("Access-Control-Allow-Origin", "*")                          // 设置允许跨域的域名列表
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")        // 设置允许跨域的请求方式
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,X-token,uuid") // 设置允许的请求头部
 		// 对于预检请求（OPTIONS），直接返回成功
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
 		// 继续执行下一个处理程序
 		next.ServeHTTP(w, r)
 	})
@@ -313,15 +397,15 @@ func respErr(w http.ResponseWriter, data string) {
 }
 
 /*---------qrcode--------------*/
-func createQrcodeUrl(uuid string) (string, error) {
+func createQrcodeUrl(uuid string) error {
 	fileName := fmt.Sprintf("cj_qrcode/%s.png", uuid)
 
 	// 生成二维码
 	err := qrcode.WriteFile(fileName, qrcode.Medium, 256, fileName)
 	if err != nil {
-		return "", err
+		return err
 	}
-	return "http://www.cj.yirisanqiu.com/" + fileName, nil
+	return nil
 }
 func getQrcodeUrl(uuid string) string {
 	k := "cj_qrcode-" + uuid
@@ -329,121 +413,125 @@ func getQrcodeUrl(uuid string) string {
 	if found {
 		return fmt.Sprintf("%v", value)
 	}
-	url, err := createQrcodeUrl(uuid)
+	err := createQrcodeUrl(uuid)
 	if err != nil {
 		fmt.Println("createQrcodeUrl", err.Error())
 		return ""
 	}
-	cache.Store(k, url)
-	return url
+	cache.Store(k, uuid)
+	return fmt.Sprintf("cj_qrcode/%s.png", uuid)
 }
 func delQrcodeUrl(uuid string) {
-	k := "cj_qrcode" + uuid
+	k := "cj_qrcode-" + uuid
 	cache.Delete(k)
 	os.Remove(fmt.Sprintf("cj_qrcode/%s.png", uuid))
 }
 
-/* ---------ws------------*/
-type ClientManager struct {
-	Clients    map[string]*WsClient
-	Broadcast  chan []byte
-	Register   chan *WsClient
-	Unregister chan *WsClient
-}
-
-type WsClient struct {
-	Socket *websocket.Conn
-	Send   chan []byte
-	UUID   string
-}
-
-var Manager = ClientManager{
-	Register:   make(chan *WsClient),
-	Unregister: make(chan *WsClient),
-	Clients:    make(map[string]*WsClient),
-}
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("Upgrade:", err)
-		return
-	}
-	defer conn.Close()
-	uuid := r.Header.Get("uuid")
-	//可以添加用户信息验证
-	client := &WsClient{
-		Socket: conn,
-		Send:   make(chan []byte),
-		UUID:   uuid,
-	}
-
-	Manager.Register <- client
-	go client.Read()
-	go client.Write()
-}
-func (c *WsClient) Read() {
-	defer func() {
-		Manager.Unregister <- c
-		_ = c.Socket.Close()
-	}()
-	for {
-		c.Socket.PongHandler()
-		_, message, err := c.Socket.ReadMessage()
-		if err != nil {
-			_ = c.Socket.Close()
-			break
-		}
-		fmt.Println(string(message))
-		if len(message) == 0 {
-			c.Send <- message
-			continue
-		}
-		fmt.Println(string(message))
-	}
-}
-func (c *WsClient) Write() {
-	defer func() {
-		Manager.Unregister <- c
-		_ = c.Socket.Close()
-	}()
-	for {
-		select {
-		case message, ok := <-c.Send:
-			if !ok {
-				_ = c.Socket.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
-			_ = c.Socket.WriteMessage(websocket.TextMessage, message)
-		}
-	}
-}
-
-// WSStart is  项目运行前, 协程开启start -> go Manager.Start()
-func (manager *ClientManager) WSStart() {
-	for {
-		select {
-		case conn := <-Manager.Register:
-			Manager.Clients[conn.UUID] = conn
-		case conn := <-Manager.Unregister:
-			if _, ok := Manager.Clients[conn.UUID]; ok {
-				close(conn.Send)
-				delete(Manager.Clients, conn.UUID)
-			}
-		}
-	}
-}
-func SendToWeb(uuid string, data interface{}) {
-	for _, c := range Manager.Clients {
-		if c.UUID == uuid {
-			b, _ := json.Marshal(data)
-			c.Send <- b
-		}
-	}
-}
+//
+///* ---------ws------------*/
+//type ClientManager struct {
+//	Clients    map[string]*WsClient
+//	Broadcast  chan []byte
+//	Register   chan *WsClient
+//	Unregister chan *WsClient
+//}
+//
+//type WsClient struct {
+//	Socket *websocket.Conn
+//	Send   chan []byte
+//	UUID   string
+//}
+//
+//var Manager = ClientManager{
+//	Register:   make(chan *WsClient),
+//	Unregister: make(chan *WsClient),
+//	Clients:    make(map[string]*WsClient),
+//}
+//
+//var upgrader = websocket.Upgrader{
+//	CheckOrigin: func(r *http.Request) bool {
+//		return true
+//	},
+//}
+//
+//func wsHandler(w http.ResponseWriter, r *http.Request) {
+//	conn, err := upgrader.Upgrade(w, r, nil)
+//	if err != nil {
+//		log.Println("Upgrade:", err)
+//		return
+//	}
+//	defer conn.Close()
+//	uuid := r.Header.Get("uuid")
+//	//可以添加用户信息验证
+//	client := &WsClient{
+//		Socket: conn,
+//		Send:   make(chan []byte),
+//		UUID:   uuid,
+//	}
+//
+//	Manager.Register <- client
+//	go client.Read()
+//	go client.Write()
+//}
+//
+//func (c *WsClient) Read() {
+//	defer func() {
+//		Manager.Unregister <- c
+//		_ = c.Socket.Close()
+//	}()
+//	for {
+//		c.Socket.PongHandler()
+//		_, message, err := c.Socket.ReadMessage()
+//		if err != nil {
+//			_ = c.Socket.Close()
+//			break
+//		}
+//		fmt.Println(string(message))
+//		if len(message) == 0 {
+//			c.Send <- message
+//			continue
+//		}
+//		fmt.Println(string(message))
+//	}
+//}
+//
+//func (c *WsClient) Write() {
+//	defer func() {
+//		Manager.Unregister <- c
+//		_ = c.Socket.Close()
+//	}()
+//	for {
+//		select {
+//		case message, ok := <-c.Send:
+//			if !ok {
+//				_ = c.Socket.WriteMessage(websocket.CloseMessage, []byte{})
+//				return
+//			}
+//			_ = c.Socket.WriteMessage(websocket.TextMessage, message)
+//		}
+//	}
+//}
+//
+//// WSStart is  项目运行前, 协程开启start -> go Manager.Start()
+//func (manager *ClientManager) WSStart() {
+//	for {
+//		select {
+//		case conn := <-Manager.Register:
+//			Manager.Clients[conn.UUID] = conn
+//		case conn := <-Manager.Unregister:
+//			if _, ok := Manager.Clients[conn.UUID]; ok {
+//				close(conn.Send)
+//				delete(Manager.Clients, conn.UUID)
+//			}
+//		}
+//	}
+//}
+//
+//func SendToWeb(uuid string, data interface{}) {
+//	for _, c := range Manager.Clients {
+//		if c.UUID == uuid {
+//			b, _ := json.Marshal(data)
+//			c.Send <- b
+//		}
+//	}
+//}
